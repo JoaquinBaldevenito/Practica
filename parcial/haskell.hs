00e1d1801132b6ab6110cf3ce56abba01d2c8ff2
JoaquinBaldevenito <joaquinbaldevenito@gmail.com>
2024-04-19 14:45:15 -0300

    Ej sanciones y permutaciones

--- parcial/haskell.hs
+++ parcial/haskell.hs
@@ -41,6 +41,47 @@ slowSort xs = ord (permutations xs)
 
 ord :: Ord a => [[a]] -> [a]
 ord [] = []
-ord (x:xs)  | sorted x = x 
-            | otherwise = ord xs 
+ord (x:xs)  | sorted x = x
+            | otherwise = ord xs
 
+subconjuntos :: [a] -> [[a]]
+subconjuntos [] = [[]]
+subconjuntos (x:xs) = subconjuntos xs ++ map (x:) (subconjuntos xs)
+
+permutaciones [] = [[]]
+permutaciones xs = [(y:p)|y<-xs, p<- permutaciones (filter (/= y) xs)]
+
+
+intercalado :: [Int]-> [Int] ->[Int] -> Bool
+intercalado xs [] ps = xs==ps
+intercalado [] ys ps = ys==ps
+intercalado xs ys [] = False
+intercalado (x:xs) (y:ys) (p:ps)
+        | x/=p && y/=p = False
+        | x/=p && y==p = intercalado (x:xs) ys ps
+        | x==p && y/=p = intercalado xs (y:ys) ps
+        | x==p && y==p = intercalado (x:xs) ys ps || intercalado xs (y:ys) ps
+
+
+
+tareasPerm :: [(Int,Int)] -> [(Int,Int)]
+tareasPerm xs = tareas (permutaciones xs) [] maxBound 
+        
+
+tareas :: [[(Int,Int)]] -> [(Int,Int)] -> Int ->[(Int,Int)]
+tareas [] tmin min = tmin
+tareas (x:ps) tmin min  | aux x 1 < min = tareas ps x (aux x 1)  
+                        | otherwise = tareas ps tmin min
+
+aux :: [(Int,Int)] -> Int -> Int
+aux [] dia = 0
+aux (x:xs) dia  | fst x >= dia = aux xs (dia+1)
+                |otherwise = snd x + aux xs (dia+1)
+
+tareas2 :: [[(Int,Int)]] -> Int ->Int
+tareas2 []  min = min
+tareas2 (x:ps)  min  | aux x 1 < min = tareas2 ps  (aux x 1)  
+                        | otherwise = tareas2 ps min
+
+tareasPerm2 :: [(Int,Int)] -> Int
+tareasPerm2 xs = tareas2 (permutaciones xs) maxBound 
